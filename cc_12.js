f7cdd03a83e76c5efea721e47ad33bb6d902f14e
Pablo Chavez <plchavez@usf.edu>
2025-03-02 20:22:00 -0500

    Task 1 - Created Revenue Metric Card

--- cc_12.js
+++ cc_12.js
@@ -5,4 +5,4 @@ const metricCard = document.createElement(`div`); //Creating a new <div> element
 metricCard.setAttribute("class", "metric-card");//assigning class, metric card
 metricCard.setAttribute("id", "revenueCard");//id, revenueCard
 revenueCard.textContent = "Revenue: $0"//using textContent to Populate the Card
-dashboard.appendChile(metricCard)//Append the new metric card to the dashboard using appendchild
+dashboard.appendChile(metricCard);//Append the new metric card to the dashboard using appendchild
