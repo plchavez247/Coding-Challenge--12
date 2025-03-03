4110cf9dabd7804c1c8efd2e1a7ac4406ae7c3bf
Pablo Chavez <plchavez@usf.edu>
2025-03-02 20:47:27 -0500

    Task 2 - Updated Metric Cards via Array Conversion

--- cc_12.js
+++ cc_12.js
@@ -1,10 +1,16 @@
 //Task 1: Business Dashboard
 const dashboard = document.getElementById(`dashboard`);
 const dashboardSelector = document.querySelector(`#dashboard`);//selecting the dashboard container
-const RevnueCard = document.createElement(`div`); //Creating a new <div> element
-RevnueCard.setAttribute("class", "metric-card");//assigning class, metric card
-RevnueCard.setAttribute("id", "revenueCard");//id, revenueCard
-RevnueCard.textContent = "Revenue: $0"//using textContent to Populate the Card
-dashboard.appendChild(RevnueCard);//Append the new metric card to the dashboard using appendchild
+const revenueCard = document.createElement(`div`); //Creating a new <div> element
+revenueCard.setAttribute("class", "metric-card");//assigning class, metric card
+revenueCard.setAttribute("id", "revenueCard");//id, revenueCard
+revenueCard.textContent = "Revenue: $0"//using textContent to Populate the Card
+dashboard.appendChild(revenueCard);//Append the new metric card to the dashboard using appendchild
 
-//Task 2
+//Task 2: Updating Dashboard Metrics- Working with NodeLists and Arrays
+const  metricCardList = document.querySelectorAll(".metric-card");// selecting all elements with the metric card class
+const metricCardArray = Array.from(metricCardList);//Array craeted using Nodelist
+metricCardArray.forEach(card =>{
+    card.textContent += " - Updated";//updating inner test
+    card.setAttribute("style", "background color: lightblue;");//modifying style
+});
