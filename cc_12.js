0342c28015a49492eef277c4f68effe9fa34baf1
Pablo Chavez <plchavez@usf.edu>
2025-03-02 20:35:23 -0500

    Changing task 1 again

--- cc_12.js
+++ cc_12.js
@@ -1,8 +1,10 @@
 //Task 1: Business Dashboard
 const dashboard = document.getElementById(`dashboard`);
 const dashboardSelector = document.querySelector(`#dashboard`);//selecting the dashboard container
-const metricCard = document.createElement(`div`); //Creating a new <div> element
-metricCard.setAttribute("class", "metric-card");//assigning class, metric card
-metricCard.setAttribute("id", "revenueCard");//id, revenueCard
-metricCard.textContent = "Revenue: $0"//using textContent to Populate the Card
-dashboard.appendChild(metricCard);//Append the new metric card to the dashboard using appendchild
+const RevnueCard = document.createElement(`div`); //Creating a new <div> element
+RevnueCard.setAttribute("class", "metric-card");//assigning class, metric card
+RevnueCard.setAttribute("id", "revenueCard");//id, revenueCard
+RevnueCard.textContent = "Revenue: $0"//using textContent to Populate the Card
+dashboard.appendChild(RevnueCard);//Append the new metric card to the dashboard using appendchild
+
+//Task 2
